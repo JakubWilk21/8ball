1320fdb721a57241b82d1233f8b914338ffaf031
Jakub <Wilk>
2022-12-12 09:55:20 -0600

    Initial Commit

---
+++ 8ball/_ballApp.swift
@@ -0,0 +1,17 @@
+//
+//  _ballApp.swift
+//  8ball
+//
+//  Created by Jakub Wilk on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct _ballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
